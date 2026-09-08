a6c5e2349af593ddd58af8d5452b565f8a40e237
dvrcky <kingkingzot@gmail.com>
2026-09-08 22:27:25 +0400

    fix: nixvim statusline

--- nixvim/plugins/lualine.nix
+++ nixvim/plugins/lualine.nix
@@ -18,7 +18,7 @@
 
        # Show active language server
         {
-          name.__raw = ''
+          __unkeyed-1.__raw = ''
             function()
                 local msg = ""
                 local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
